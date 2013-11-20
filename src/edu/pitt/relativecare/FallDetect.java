45b38592b81e0fd9a4cf8ca509248895bec39621
Jiaxin Shan <seedjeffwan@gmail.com>
2013-11-20 01:40:44 -0500

    Refactor FallDetect and then Let Lihua take over this module.

--- src/edu/pitt/relativecare/FallDetect.java
+++ src/edu/pitt/relativecare/FallDetect.java
@@ -22,19 +22,19 @@ public class FallDetect extends Activity implements SensorEventListener {
     private static final String TAG = "FallDetect";
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private float mLastX, mLastY, mLastZ;
-    private boolean mInitialized;
-    private final float NOISE = (float) 2.0;
-
-
+    private float x, y, z;
+    private TextView tvX, tvY, tvZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_falldetect);
+        tvX = (TextView) findViewById(R.id.x_axis);
+        tvY = (TextView) findViewById(R.id.y_axis);
+        tvZ = (TextView) findViewById(R.id.z_axis);
+
 
         Log.i(TAG, "enter me fall detect");
-        mInitialized = false;
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -45,14 +45,11 @@ public class FallDetect extends Activity implements SensorEventListener {
                 mSensor = deviceSensor.get(i);
                 Log.i(TAG, mSensor.getName());
             }
-
-
         } else {
             //Sorry, There are no accelerometers on your device
         }
 
         mSensorManager.registerListener(this, mSensor ,SensorManager.SENSOR_DELAY_NORMAL);
-
 
     }
 
@@ -75,55 +72,31 @@ public class FallDetect extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        // The accelerometer return 3 value , each one for each axis.
-        TextView tvX= (TextView)findViewById(R.id.x_axis);
-        TextView tvY= (TextView)findViewById(R.id.y_axis);
-        TextView tvZ= (TextView)findViewById(R.id.z_axis);
-        ImageView iv = (ImageView)findViewById(R.id.image);
+        // If the sensor data is unreliable, return
+        if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE)
+            return;
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                //get acceleration vector data
+                x = event.values[0];
+                y = event.values[0];
+                z = event.values[0];
 
+                Log.i(TAG,"x: "+ x);
+                Log.i(TAG,"y: "+ y);
+                Log.i(TAG,"z: "+ z);
 
-
-
-        final float alpha = (float) 0.8;
-
-
-        float x = event.values[0];
-        float y = event.values[0];
-        float z = event.values[0];
-
-        if (!mInitialized) {
-            mLastX = x;
-            mLastY = y;
-            mLastZ = z;
-            tvX.setText("0.0");
-            tvY.setText("0.0");
-            tvZ.setText("0.0");
-            mInitialized = true;
-
-        } else {
-
-            float deltaX = Math.abs(mLastX - x);
-            float deltaY = Math.abs(mLastY - y);
-            float deltaZ = Math.abs(mLastZ - z);
-            if (deltaX < NOISE) deltaX = (float)0.0;
-            if (deltaY < NOISE) deltaY = (float)0.0;
-            if (deltaZ < NOISE) deltaZ = (float)0.0;
-            mLastX = x;
-            mLastY = y;
-            mLastZ = z;
-            tvX.setText(Float.toString(deltaX));
-            tvY.setText(Float.toString(deltaY));
-            tvZ.setText(Float.toString(deltaZ));
-            iv.setVisibility(View.VISIBLE);
-            if (deltaX > deltaY) {
-                iv.setImageResource(R.drawable.horizontal);
-            } else if (deltaY > deltaX) {
-                iv.setImageResource(R.drawable.vertical);
-            } else {
-                iv.setVisibility(View.INVISIBLE);
-            }
-
+                //
+                tvX.setText(Float.toString(x));
+                tvY.setText(Float.toString(y));
+                tvZ.setText(Float.toString(z));
         }
+
+
+
+
+
+
 
     }
 
